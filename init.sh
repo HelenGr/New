09f9d570b446a9d38deaae45f31766ae64d867b5
HelenGr <51291313+HelenGr@users.noreply.github.com>
2019-06-05 17:04:54 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-available/default
+sudo ln -sf /home/box/web/etc/nginx.conf/etc/nginx/sites-enadled/rest.conf
+sido rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart

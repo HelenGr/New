c18a9ac6b9e2980ebeac4a197821f274a527a1c4
HelenGr <51291313+HelenGr@users.noreply.github.com>
2019-06-06 21:02:34 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/django.py /etc/gunicorn.d/django.py
+# sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
 sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/mysql start
